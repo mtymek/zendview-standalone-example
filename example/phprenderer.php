42ddac0604e4b1422e577d837efb8b4a9605266e
Mateusz Tymek <mtymek@gmail.com>
2013-11-11 13:28:59 +0100

    updated example

--- example/phprenderer.php
+++ example/phprenderer.php
@@ -6,13 +6,14 @@ use Zend\View\Resolver\TemplatePathStack;
 
 include __DIR__ . '/../bootstrap.php';
 
+// configuration
 $resolver = new TemplatePathStack(array(
     'script_paths' => array('view')
 ));
 $phpRenderer = new PhpRenderer();
 $phpRenderer->setResolver($resolver);
 
+// example usage
 $viewModel = new ViewModel(array('userName' => 'John Doe'));
 $viewModel->setTemplate('index.phtml');
-
 echo $phpRenderer->render($viewModel);
